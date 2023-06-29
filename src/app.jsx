8614439d74d9ccb305fb00f1e9b6fd03786a828e
Juntao Qiu <juntao.qiu@gmail.com>
2023-06-29 16:01:14 +1000

    click button to render another quote

--- src/app.jsx
+++ src/app.jsx
@@ -1,9 +1,40 @@
-import React from 'react';
+import React, { useState } from "react";
+
+const quotes = [
+  {
+    content:
+      "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
+    author: "Martin Fowler",
+  },
+  {
+    content: "Truth can only be found in one place: the code.",
+    author: "Robert C. Martin",
+  },
+  {
+    content:
+      "Optimism is an occupational hazard of programming: feedback is the treatment.",
+    author: "Kent Beck",
+  },
+];
+
 const App = () => {
-  return (<div>
-    <p className="content">If you want to lift yourself up, lift up someone else.</p>
-    <p><span className="author">&mdash; Booker T. Washington</span></p>
-  </div>)
-}
+  const [index, setIndex] = useState(0);
+
+  const clickHandler = () => {
+    setIndex((index) => (index + 1) % 3);
+  };
+
+  return (
+    <>
+      <div>
+        <p className="content">{quotes[index].content}</p>
+        <p>
+          <span className="author">&mdash; {quotes[index].author}</span>
+        </p>
+      </div>
+      <button onClick={clickHandler}>next</button>
+    </>
+  );
+};
 
 export default App;
