8001428f6f561128a8049f2bb573e49eb137d857
Juntao Qiu <juntao.qiu@gmail.com>
2023-06-29 15:32:18 +1000

    --watch

--- src/index.jsx
+++ src/index.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './app';
 
+import './style.css';
+
 const domNode = document.getElementById('root');
 const root = createRoot(domNode);
 
