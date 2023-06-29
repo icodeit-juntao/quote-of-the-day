c378b3b82d34c94a4a2a22f64fbac79524e2413d
Juntao Qiu <juntao.qiu@gmail.com>
2023-06-29 14:04:06 +1000

    added esbuild and react

---
+++ src/app.jsx
@@ -0,0 +1,8 @@
+const App = () => {
+  return (<div>
+    <p className="content">If you want to lift yourself up, lift up someone else.</p>
+    <p><span className="author">&mdash; Booker T. Washington</span></p>
+  </div>)
+}
+
+export default App;
